9536900fd42872e1321473058469786b0b4924a9
Aleksey <poltavaaa@gmail.com>
2022-04-29 17:57:37 +0300

    Seed database
    1.02

---
+++ src/lib/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// import file for seeding database
+import { seedDatabase } from "../seed";
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC77MZSzUQJuAhCTLFLpKiI3mlyQWDEiSc",
+  authDomain: "instagram-4bc6a.firebaseapp.com",
+  projectId: "instagram-4bc6a",
+  storageBucket: "instagram-4bc6a.appspot.com",
+  messagingSenderId: "623662632878",
+  appId: "1:623662632878:web:f6445d6e4a3b0b1905023f"
+};
+
+
+// Initialize Firebase
+
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore(firebase);
+const {FieldValue} = db;
+
+// call seed file once
+
+export {firebase, FieldValue};
